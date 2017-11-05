e3cf5299590ff6d6a53b95d42d4f882599216eae
Derek Ju <derekju@uber.com>
2017-11-04 17:49:28 -0700

    Update AI card

--- src/components/AiCard.js
+++ src/components/AiCard.js
@@ -1,7 +1,26 @@
-import React from 'react';
-import './AiCard.css';
+// @flow
 
-class AiCard extends React.Component {
+import {IMPERIAL_BLUE} from '../styles/colors';
+import React from 'react';
+
+const styles = {
+  base: {
+    border: '2px solid black',
+    width: '267px',
+    height: '300px',
+  },
+  commandContainer: {
+    fontSize: '13px',
+    padding: '5px',
+  },
+  header: {
+    backgroundColor: IMPERIAL_BLUE,
+    color: 'white',
+    padding: '5px',
+  },
+};
+
+class AiCard extends React.Component<{}> {
   renderCommand(key, condition, command) {
     return (
       <div key={key}>
@@ -15,9 +34,9 @@ class AiCard extends React.Component {
     const baseClass = this.props.elite ? 'AiCardElite' : 'AiCard';
 
     return (
-      <div className={baseClass}>
-        <div className={`${baseClass}-header`}>{this.props.name}</div>
-        <div className="AiCard-command-container">
+      <div style={styles.base}>
+        <div style={styles.header}>{this.props.name}</div>
+        <div style={styles.commandContainer}>
           {this.props.commands.map((command, index) =>
             this.renderCommand(`${this.props.name}-${index}`, command.condition, command.command)
           )}
