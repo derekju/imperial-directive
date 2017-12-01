73009f2b3574f95751830fc949486bfe28adc976
Derek Ju <derek.ju@gmail.com>
2017-12-01 12:30:29 -0800

    Brushfire: Add intro modal before optional deployment

--- src/reducers/missions/brushfire.js
+++ src/reducers/missions/brushfire.js
@@ -255,6 +255,13 @@ function* handleSpecialSetup(): Generator<*, *, *> {
     helperInitialSetup,
     '{ELITE}AT-ST{END}, E-Web Engineer, Imperial Officer, Probe Droid'
   );
+  yield call(helperEventModal, {
+    text: [
+      'The threat has been increased.',
+      'An optional deployment will now be done.',
+    ],
+    title: 'Initial Setup',
+  });
   // Double current threat
   yield call(helperIncreaseThreat, 2);
   // Do optional deployment
