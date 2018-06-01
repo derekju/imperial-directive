15f967f311ae3b5aec41f3ec9825842eb830bae3
Derek Ju <derek.ju@gmail.com>
2018-05-31 17:55:42 -0700

    Add tile info to Aftermath

--- src/reducers/missions/aftermath.js
+++ src/reducers/missions/aftermath.js
@@ -162,8 +162,8 @@ function* handleFortifiedEvent(): Generator<*, *, *> {
         REFER_CAMPAIGN_GUIDE,
         ['An E-Web Engineer, Stormtrooper group, and an Imperial Officer will now be deployed.'],
         ['eWebEngineer', 'Deploy to the Yellow deployment point in the Atrium. The figure also becomes focused.'],
-        ['stormtrooper', 'Deploy to the right side of the Storage room.'],
-        ['imperialOfficer', 'Deploy to the right side of the Storage room.'],
+        ['stormtrooper', 'Deploy to the right side of the Storage room (tile 22B).'],
+        ['imperialOfficer', 'Deploy to the right side of the Storage room (tile 22B).'],
       );
       // PRIORITY TARGET SWITCH #2
       const {priorityTargetKillHero} = yield select(getState);
