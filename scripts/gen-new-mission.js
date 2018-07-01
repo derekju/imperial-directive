a67b04950a21dc61add71b0e7db81c2dfe687378
Derek Ju <derek.ju@gmail.com>
2018-07-01 09:08:00 -0700

    Modify copied template when generating new mission

--- scripts/gen-new-mission.js
+++ scripts/gen-new-mission.js
@@ -1,6 +1,7 @@
 // @flow
 
 const fs = require('fs');
+const replace = require('replace-in-file');
 const upperFirst = require('lodash/upperFirst');
 
 const missionName = process.argv[2];
@@ -25,6 +26,33 @@ fs.appendFileSync(`./src/reducers/index.js`, `import ${missionName} from './miss
 console.log('Appending to reducers/types.js...');
 fs.appendFileSync(`./src/reducers/types.js`, `import type {${upperFirst(missionName)}StateType} from './missions/${missionName}';\n`);
 fs.appendFileSync(`./src/reducers/types.js`, `  ${missionName}: ${upperFirst(missionName)}StateType,\n`);
+
+console.log('Modifying template...');
+replace.sync({
+  files: `./src/reducers/missions/${missionName}.js`,
+  from: 'export function* forestAmbush(): Generator',
+  to: 'export function* binaryRevolution(): Generator',
+});
+replace.sync({
+  files: `./src/reducers/missions/${missionName}.js`,
+  from: "const MISSION_NAME = 'forestAmbush';",
+  to: `const MISSION_NAME = '${missionName}';`,
+});
+replace.sync({
+  files: `./src/reducers/missions/${missionName}.js`,
+  from: 'export type ForestAmbushStateType',
+  to: `export type ${upperFirst(missionName)}StateType`,
+});
+replace.sync({
+  files: `./src/reducers/missions/${missionName}.js`,
+  from: 'export default (state: ForestAmbushStateType',
+  to: `export default (state: ${upperFirst(missionName)}StateType`,
+});
+replace.sync({
+  files: `./src/reducers/missions/${missionName}.js`,
+  from: 'export const getForestAmbushGoalText',
+  to: `export const get${upperFirst(missionName)}GoalText`,
+});
 
 // Done
 process.exit(0);
